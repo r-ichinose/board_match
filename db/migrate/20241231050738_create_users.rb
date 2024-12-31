4919eef401532c34c8e27dc97192380f50c2fd5b
r-ichinose <reoyinolai719@gmail.com>
2024-12-31 23:02:01 +0900

    fix migration

--- db/migrate/20241231050738_create_users.rb
+++ db/migrate/20241231050738_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string  :nickname,                 null: false
-      t.string  :email,                    null: false
+      t.string  :email,                    null: false, limit: 191
       t.string  :encrypted_password,       null: false
       t.integer :height,                   null: false
       t.integer :weight,                   null: false
